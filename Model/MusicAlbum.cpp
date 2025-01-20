06a1ffa9d89af272b1ceab517af37cbbf59ae211
Nicoló <48124723+Nico123abc@users.noreply.github.com>
2025-01-20 22:06:07 +0100

    Create MusicAlbum.cpp
    #include "MusicAlbum.h"
    #include "Visitor.h"
    
    void MusicAlbulm::accept(Visitor* visitor) {
        visitor->visit(*this);
    }
    
    bool MusicAlbulm::mathes(const QString& s) const {
        return mTitle.contains(s, Qt::CaseInsensitive)||
             mArtist.contains(s, Qt::CaseInsensitive)||
             mGenre.contains(s, Qt::CaseInsensitive);
    }

---
+++ Model/MusicAlbum.cpp
@@ -0,0 +1,12 @@
+#include "MusicAlbum.h"
+#include "Visitor.h"
+
+void MusicAlbulm::accept(Visitor* visitor) {
+    visitor->visit(*this);
+}
+
+bool MusicAlbulm::mathes(const QString& s) const {
+    return mTitle.contains(s, Qt::CaseInsensitive)||
+         mArtist.contains(s, Qt::CaseInsensitive)||
+         mGenre.contains(s, Qt::CaseInsensitive);
+}
