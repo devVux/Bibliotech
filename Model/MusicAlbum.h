da95377db9f27d2d51b82d49d1e18cd5cf2df9d9
Nicoló <48124723+Nico123abc@users.noreply.github.com>
2025-01-20 22:07:03 +0100

    Create MusicAlbum.h
    #pragma once
    #include "Media.h"
    
    class MusicAlbulm : public Media {
    
    private:
        QString mArtist;
        QString mGenre;
        uint32_t mTrackCount;
    
    
    public:
        MusicAlbulm(const QString& title,const QString& publisher,uint32_t year, const QString& artist, const QString& genre, uint32_t trackCount)
            : Media(title, publisher, year), mArtist(artist),  mGenre(genre), mTrackCount(trackCount) { }
    
        virtual void accept(Visitor* visitor) override;
    
       
        virtual bool mathes(const QString& s) const override;
    
        void artist(const QString& artist) { mArtist = artist; mDirty = true; }
        void genre(const QString& genre)   { mGenre  = genre;  mDirty = true; }
        void trackCount(uint32_t count)    { mTrackCount = count; mDirty = true; }
    
        const QString& artist() const    { return mArtist; }
        const QString& genre() const     { return mGenre; }
        uint32_t trackCount() const      { return mTrackCount; }
    
    };

---
+++ Model/MusicAlbum.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Media.h"
+
+class MusicAlbulm : public Media {
+
+private:
+    QString mArtist;
+    QString mGenre;
+    uint32_t mTrackCount;
+
+
+public:
+    MusicAlbulm(const QString& title,const QString& publisher,uint32_t year, const QString& artist, const QString& genre, uint32_t trackCount)
+        : Media(title, publisher, year), mArtist(artist),  mGenre(genre), mTrackCount(trackCount) { }
+
+    virtual void accept(Visitor* visitor) override;
+
+   
+    virtual bool mathes(const QString& s) const override;
+
+    void artist(const QString& artist) { mArtist = artist; mDirty = true; }
+    void genre(const QString& genre)   { mGenre  = genre;  mDirty = true; }
+    void trackCount(uint32_t count)    { mTrackCount = count; mDirty = true; }
+
+    const QString& artist() const    { return mArtist; }
+    const QString& genre() const     { return mGenre; }
+    uint32_t trackCount() const      { return mTrackCount; }
+
+};
+
