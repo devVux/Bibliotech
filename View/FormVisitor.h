76f4ebf60156a70aab6d077f9443a33de0c8df96
Nicoló <48124723+Nico123abc@users.noreply.github.com>
2025-01-21 19:17:26 +0100

    Update FormVisitor.h

--- View/FormVisitor.h
+++ View/FormVisitor.h
@@ -13,7 +13,7 @@ class FormVisitor: public Visitor {
 		
 		virtual void visit(Film& film) override;
 		virtual void visit(Book& book) override;
-		virtual void visit(MusicAlbulm& album) override;  //
+		virtual void visit(MusicAlbum& album) override;  //
 
 		QDialog* dialog() const { return pDialog; }
 
